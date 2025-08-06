64d0ab1445fb2e0bd4cc97df71e0c8fb438ba2a5
José Schmidt <ze.schmidt@gmail.com>
2025-08-06 17:07:33 -0300

    fix: usar porta correta para Render

--- app.py
+++ app.py
@@ -46,4 +46,5 @@ def estoque():
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
